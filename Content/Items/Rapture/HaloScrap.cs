4ac72f52ab6d611ac2a1c3f3e5e150d9a4efb985
vaydr <vramkumar369@gmail.com>
2026-02-20 22:13:53 -0800

    retextures

--- Content/Items/Rapture/HaloScrap.cs
+++ Content/Items/Rapture/HaloScrap.cs
@@ -1,3 +1,5 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
@@ -6,8 +8,6 @@ namespace VanillaPlus.Content.Items.Rapture
 {
     public class HaloScrap : ModItem
     {
-        public override string Texture => $"Terraria/Images/Item_{ItemID.HallowedBar}";
-
         public override void SetDefaults()
         {
             Item.width = 20;
@@ -15,6 +15,14 @@ namespace VanillaPlus.Content.Items.Rapture
             Item.maxStack = 9999;
             Item.value = Item.buyPrice(silver: 20);
             Item.rare = ItemRarityID.LightRed;
+
+            Item.scale = 1.5f;
+        }
+
+        public override bool PreDrawInInventory(SpriteBatch spriteBatch, Vector2 position, Rectangle frame, Color drawColor, Color itemColor, Vector2 origin, float scale)
+        {
+            spriteBatch.Draw(ModContent.Request<Texture2D>(Texture).Value, position, frame, drawColor, 0f, origin, scale * 1.5f, SpriteEffects.None, 0f);
+            return false;
         }
     }
 }
